590508c008b7503a01a978c61dc3ebf4a8be73cb
Mona Lisa <mona@Lisa.com>
2023-03-22 09:30:20 +0100

    22-03 09:29 commit 2 alle files

--- site/index.php
+++ site/index.php
@@ -1,5 +1,18 @@
 <?php
-
-// Dit is het startpunt van je applicatie.
 require 'database.php';
-echo 'Welkom bij het receptenboek';
+$sql = "SELECT * FROM Recepten_Database";
+$result = mysqli_query($conn,$sql);
+$all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receptenboek</title>
+</head>
+<body>
+    <h1></h1>
+</body>
+</html>
