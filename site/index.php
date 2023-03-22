c3bb633b5a8812b675ce19091dec4104ad420157
Mona Lisa <mona@Lisa.com>
2023-03-22 11:10:25 +0100

    22-03 11:09 commit 3: fotos/startpagina

--- site/index.php
+++ site/index.php
@@ -4,6 +4,7 @@ $sql = "SELECT * FROM Recepten_Database";
 $result = mysqli_query($conn,$sql);
 $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +12,14 @@ $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receptenboek</title>
+    <link href="style.css">
 </head>
 <body>
-    <h1></h1>
+    <h1>Welkom bij receptenboek èpicer!</h1>
+
+    <?php foreach($all_recepten as $recept) { ?>
+    <img class= "fotos" src="images/<?php echo $recept['Foto']?>" alt="img">  
+    <?php } ?>
+
 </body>
 </html>
